a017d5fc3bd91c8f3926c25561c47e73cf5ca233
Dany <danielhakmeh@gmail.com>
2025-08-30 19:05:42 +0300

    Model Trainer

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.Components.data_transformation import DataTransformation
 from src.Components.data_transformation import DataTransformationConfig
+from src.Components.model_trainer import ModelTrainer
+from src.Components.model_trainer import ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -41,8 +43,13 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e, sys)
+
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    model_trainer = ModelTrainer()
+    r2_square = model_trainer.initiate_model_trainer(train_arr, test_arr)
+    print(f"R2 square value: {r2_square}")
