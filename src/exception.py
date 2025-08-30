bb84a8afc6f54c87fa77260efbf3783be6d428ba
Dany <danielhakmeh@gmail.com>
2025-08-30 15:05:14 +0300

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,25 +1,28 @@
 import sys
 import logging
-from logger import logging
+from typing import Any
 
-def error_message_detail(error, error_detail: sys):
+def error_message_detail(error: Exception, error_detail: Any) -> str:
     _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    line_number = exc_tb.tb_lineno
-    error_message = f"Error occurred in script: {file_name} at line number: {line_number} with message: {str(error)}"
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+        error_message = f"Error occurred in script: {file_name} at line number: {line_number} with message: {str(error)}"
+    else:
+        error_message = f"Error occurred: {str(error)}"
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
+    def __init__(self, error_message: Exception, error_detail: Any):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.error_message
 
 if __name__ == "__main__":
     try:
         a = 1 / 0
-    except Exception as e:      
+    except Exception as e:
         logging.info("Divided by zero error")
         raise CustomException(e, sys)
